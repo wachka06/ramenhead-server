c0d6973899ece4536e407f5c5dbcac86cac47d5e
wachka06 <wachka06@gmail.com>
2018-10-11 18:09:50 -0400

    api call

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -30,7 +30,11 @@
 #     end
 #   end
 
-API_KEY = NONE
+require "json"
+require "http"
+require "optparse"
+
+API_KEY = "WsnNTUNYbI9WZVvVcr_YCkEjuofNIlR80uCTnU1qDZ1N-NoWCJ2HYELVzxLOXxZPCmsvdMFxmnLZQ-NsShyL9t79lh7LkJD42JJ6W0X-7pIzxF6WbLf32JM1U_G8W3Yx"
 API_HOST = "https://api.yelp.com"
 SEARCH_PATH = "/v3/businesses/search"
 SEARCH_LIMIT = 5
