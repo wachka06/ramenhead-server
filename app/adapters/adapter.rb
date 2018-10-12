407970dcc97060276a9f784f8294b354c0226e5d
wachka06 <wachka06@gmail.com>
2018-10-12 08:51:26 -0400

    api

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -1,35 +1,3 @@
-# require 'yelp'
-#
-# module Adapter
-#   class Yelp
-#
-#     def initialize
-#       @client ||=
-#         Octokit::Client.new({ consumer_key: YOUR_CONSUMER_KEY,
-#                             consumer_secret: YOUR_CONSUMER_SECRET,
-#                             token: YOUR_TOKEN,
-#                             token_secret: YOUR_TOKEN_SECRET
-#                           })
-#     end
-#
-#     def repo_issues
-#       @client.issues("#
-#         {repo.user.github_username}/#
-#         {repo.name}")
-#     end
-#
-#     def create_repo_webhook
-#       @client.create_hook("#
-#         {repo.user.github_username}/#
-#         {repo.name}",
-#         'web',
-#         {url: "#
-#   {ENV['ISSUE_TRACKR_APP_URL']}/webhooks/receive",
-#            content_type: 'json'},
-#         {events: ['issues'], active: true})
-#     end
-#   end
-
 require "json"
 require "http"
 require "optparse"
